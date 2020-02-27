5bd08c4e48d3b60be93b1c50d008e09b06620a33
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-02-26 23:46:52 -0300

    Omnistack

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,8 +3,18 @@ const Dev = require('../Models/Dev');
 
 module.exports = {
     async index(req, res){
-        const pro = await Dev.find();
-        return res.json(pro);
+        const {user} = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+            $and:[
+                {_id: {$ne: user}},
+                {_id: {$nin: loggedDev.like}},
+                {_id: {$nin: loggedDev.dislike}},
+            ],
+        })
+        return res.json(users);
     },
     async store(req, res){
         const { username } = req.body;
